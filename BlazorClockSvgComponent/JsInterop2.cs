2ca3f001c611a148d5fe045c25093215698fe281
HP USER <HP USER@DESKTOP-HHMHCHT>
2018-08-12 23:22:10 -0400

    Add project files.

---
+++ BlazorClockSvgComponent/JsInterop2.cs
@@ -0,0 +1,19 @@
+﻿using BlazorClockSvgComponent.Classes;
+using Microsoft.JSInterop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorClockSvgComponent
+{
+    public static class JsInterop2
+    {
+
+        public static bool Run(TransferParameters _params)
+        {
+            return (JSRuntime.Current as IJSInProcessRuntime).Invoke<bool>(
+                "JsInterop2.Run", _params);
+        }
+    }
+}
